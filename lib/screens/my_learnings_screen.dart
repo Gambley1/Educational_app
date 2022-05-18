4d3749cb8666d1af42d8e26c4d95f54f99eac304
Yerassyl Tleugazy <tleugazy98@gmail.com>
2022-05-19 03:50:21 +0600

    sdsdds

--- lib/screens/my_learnings_screen.dart
+++ lib/screens/my_learnings_screen.dart
@@ -1,10 +1,15 @@
+import 'package:educational_app/screens/home/nav_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:educational_app/models/user_model.dart';
 
+import 'login_screen.dart';
 class MyLearningsScreen extends StatelessWidget {
   const MyLearningsScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: LoginScreen(),
+    );
   }
 }
